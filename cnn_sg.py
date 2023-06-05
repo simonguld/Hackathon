0c243ae40b0692fe7fc4c3d4f97f35025bdb412b
Simon Guldager <guldager.simon@gmail.com>
2023-06-05 23:30:45 +0200

    minor changes

--- cnn_sg.py
+++ cnn_sg.py
@@ -1,13 +1,13 @@
 ### SETUP -----------------------------------------------------------------------------------------------------------------------------------
 
-## Paths
-# Save mnist cnn model to the following path
-cnn_mnist_path = 'C:\\Users\\Simon\\PycharmProjects\\Projects\\Projects\\Hackathon'
+
+
 
 ## Imports:
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+import time
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'  #  {'0', '1', '2', '3'} = {Show all messages, remove info, remove info and warnings, remove all messages}
 import tensorflow as tf
@@ -15,10 +15,74 @@ import tensorflow as tf
 from tensorflow import keras
 from keras import models, layers, optimizers
 from sklearn import datasets, model_selection, metrics
-from knn_sg import cross_validation
 from PIL import Image
 
+## Change directory to current one
+dir_path = os.path.dirname(os.path.realpath(__file__))
+os.chdir(dir_path)
+cnn_mnist_path = './cnn_mnist.h5'
+
+
 ### FUNCTIONS -------------------------------------------------------------------------------------------------------------------------------
+
+def cross_validation(x_train, train_labels, method, test_fraction = 0.2, f1_score_average = 'weighted'):
+    """
+    Randomly shuffles and splits a training set into a traning and test set, whose relative size is determined by test_fraction.
+    If the training set is divided into N sets, one will act as traing set and the remaning N-1 sets will act as training data.
+    The function continues until all N sets has acted as training set and returns an f1 score for each roll.
+
+    params:
+            x_train: Training data. An nd.array of dimension (No. of points, dimension of points). 
+            train_labels: Traning data labels. An nd.array of dimension (No. of points, 1)
+            method: the machine learning classification method used to assign labels. When used as parameter to the function,
+            it must be restricted to only depend on (x_train,train_labels,x_test) in that order
+            test_fraction: The approximate fraction of training data that will act as test data in a given roll
+    returns:
+            a list of f1-scores for each roll
+    """
+  
+
+    # find number of training points
+    n_points, _ = x_train.shape
+    # find index to split data set
+    split_index = np.floor( n_points * test_fraction)
+    # find number of cross validation subsets / rolls
+    rolls = int(np.floor(n_points / (split_index)))
+
+    ## shuffle training data and corresponding labels
+    # generate random integer values from [0, no. of points]
+    row_shuffle = np.linspace(0, n_points - 1, n_points).astype('int')
+    random_generator = np.random.default_rng()
+    random_generator.shuffle(row_shuffle)
+
+    # Apply random shuffling to training data and labels
+    shuffled_data = x_train.astype('float')[row_shuffle]
+    shuffled_labels = train_labels.astype('int')[row_shuffle]
+
+    # Make list for storing f1 scores
+    f1_list = []
+
+    # Perform the roll, letting each subset act as test data
+    for i in range(rolls):
+        # The rows that will act as test data in current roll
+        index_range_test = [int (i * split_index),  int ((i + 1) * split_index)]
+
+        # Extract test_data and corresponding true labels for current roll
+        test_data = shuffled_data[index_range_test[0]:index_range_test[1]]
+        test_labels_true = shuffled_labels[index_range_test[0]:index_range_test[1]]
+
+        # Extract training data and corresponding true labels for current roll
+        train_data = np.delete(shuffled_data, np.arange(index_range_test[0], index_range_test[1]), axis = 0)
+        train_labels = np.delete(shuffled_labels, np.arange(index_range_test[0], index_range_test[1]), axis = 0)
+
+        # Assign test point labels using the provided method
+        test_labels = method(train_data, train_labels, test_data)
+
+        # Calculate and store the f1-score of current roll
+        f1_score = metrics.f1_score(test_labels_true, test_labels, average = f1_score_average)
+        f1_list.append(f1_score)
+
+    return f1_list
 
 ## Todo:
 # Kan man nøjes med at bygge en gang og så fitte på forskellige data?
@@ -87,6 +151,7 @@ def main():
                 loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
                 metrics=['acc'])
 
+        t_start = time.time()
         # Train model
         history = digit_model.fit(train_data, train_labels, epochs=10, 
                         validation_data=(test_data, test_labels))
@@ -96,7 +161,8 @@ def main():
         y_pred_bool = np.argmax(y_pred, axis = 1)
 
         print(metrics.classification_report(test_labels, y_pred_bool))
-
+        t_end = time.time()
+        print('Time elapsed: ', t_end - t_start)
     if numbers28x28:
 
 
@@ -112,9 +178,10 @@ def main():
         pixel_shape = np.empty(2)
         samples, pixel_shape[0], pixel_shape[1] = data_train.shape
 
+        print(data_train.shape, data_test.shape)
 
         build_and_train, load_model = True, False
-
+        t_start = time.time()
         ## Build and train
         if build_and_train:
             #build model
@@ -144,7 +211,7 @@ def main():
 
 
             #Train
-            history = model.fit(data_train,labels_train, epochs = 4, verbose = 1, validation_data=(data_test,labels_test))
+            history = model.fit(data_train,labels_train, epochs = 1, verbose = 1, validation_data=(data_test,labels_test))
             
             #Save
             model.save(cnn_mnist_path)
@@ -153,7 +220,8 @@ def main():
         if load_model:
             model = keras.models.load_model(cnn_mnist_path)
 
-        
+        t_end = time.time()
+        print('Time elapsed: ', t_end - t_start)
         # 'Cross validate' (not really a proper cross validation by testing on traning data on which the model was built)
         x_train, x_test, x_labels, x_test_labels = model_selection.train_test_split(data_train, labels_train, test_size = 0.2, \
             random_state = 25)
